1540af490b513cdff7031c8634f4bb609eba9a77
pingao777 <pingao777@gmail.com>
2016-01-30 12:55:40 +0800

    自动添加一条空行脚本

---
+++ BlankRat.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Today is %date:~,4%-%date:~5,2%-%date:~8,2%. Good day, sir! I am a small rat,and i like digging holes. I will put one blank line in a hole aftr i diggle it. So, people always call me BlankRat. Now, i am going to search every file of this directory to append one blank line.
+echo.
+
+for /r %%i in (*.*) do (
+    if not "%%~xi"==".bat" if not "%%~xi"==".log" (
+        echo.>>%%i
+        echo %%i
+    )
+)
+    
+pause 
